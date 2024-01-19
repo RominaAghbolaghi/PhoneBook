1f1782093b7cec421d96ed90376ab58ad618122a
Romina Aghbolaghi <romina.aghbolaghi@gmail.com>
2024-01-19 18:09:07 +0100

    third session (encapsulation part)push

--- src/com/bnp/phonebook/Contact.java
+++ src/com/bnp/phonebook/Contact.java
@@ -2,7 +2,30 @@ package com.bnp.phonebook;
 
 public class Contact {
     // attributes
-    public String name;
-    public String number;
+    private String name;
+//    public String name;
+    private String number;
+//    public String number;
 
+    private String capitilization(String name) {
+        if (name != null && !name.isEmpty()) {
+            return (Character.toUpperCase(name.charAt(0)) + name.substring(1));
+        }
+        return name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name) {
+        this.name = capitilization(name);
+//        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
--- src/com/bnp/phonebook/PhoneBook.java
+++ src/com/bnp/phonebook/PhoneBook.java
@@ -13,7 +13,7 @@ public class PhoneBook {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 //    private static ArrayList<String> contactNames = new ArrayList<String>();
 //    private static ArrayList<String> contactNumbers = new ArrayList<String>();
     static ArrayList<Contact> contacts = new ArrayList<>();
@@ -48,32 +48,39 @@ public class PhoneBook {
         System.out.print(ANSI_CYAN + "Please enter your request: " + ANSI_RESET);
     }
     private static void addContact() {
-        Contact inputContact = new Contact();
+        Contact newContact = new Contact();
         System.out.print(ANSI_CYAN + "\nEnter your contact's name: " + ANSI_RESET);
 //        String name = scanner.nextLine();
-        inputContact.name = scanner.nextLine();
+//        newContact.name = scanner.nextLine();
+        newContact.setName(scanner.nextLine());
 //        System.out.println("Your contact's name is: " + name + "\n");
-        System.out.println("Your contact's name is: " + inputContact.name + "\n");
+//        System.out.println("Your contact's name is: " + newContact.name + "\n");
+        System.out.println("Your contact's name is: " + newContact.getName()+ "\n");
         System.out.print(ANSI_CYAN + "Enter your contact's number: " + ANSI_RESET);
 //        String number = scanner.nextLine();
-        inputContact.number = scanner.nextLine();
+//        newContact.number = scanner.nextLine();
+        newContact.setNumber(scanner.nextLine());
 //        System.out.println("Your contact's number is: " + number + "\n");
-        System.out.println("Your contact's number is: " + inputContact.number + "\n");
+//        System.out.println("Your contact's number is: " + newContact.number + "\n");
+        System.out.println("Your contact's number is: " + newContact.getNumber() + "\n");
+        contacts.add(newContact);
 //        contactNames.add(name);
 //        contactNumbers.add(number);
         System.out.println("Your contact's info is added successfully!\n");
     }
     private static void printContactList() {
 //        if (contactNames.isEmpty())
-        if (contacts.isEmpty())
-            System.out.println("There is not yet any contact in your phonebook!");
-        else
+        if (!contacts.isEmpty()) {
             System.out.println("******* Phonebook list *******");
 //        for (int i = 0; i < contactNames.size(); i++)
 //            System.out.println("Name: " + contactNames.get(i) + "    Phone number: " + contactNumbers.get(i));
 //        for (int i = 0; i < contacts.size(); i++)
 //            System.out.println("Name: " + contacts.get(i).name + "    Phone number: " + contacts.get(i).number);
-        for (Contact contact : contacts)
-            System.out.println("Name: " + contact.name + "    Phone number: " + contact.number);
+            for (Contact contact : contacts)
+//                System.out.println("Name: " + contact.name + "    Phone number: " + contact.number);
+                System.out.println("Name: " + contact.getName() + "    Phone number: " + contact.getNumber());
+        } else {
+            System.out.println("There is not yet any contact in your phonebook!");
+        }
     }
 }
